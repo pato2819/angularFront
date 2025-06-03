9bf527fcab7f4e77d05167efc7f7dffc1246f847
patricia canul <patricia.canul@innovasport.com>
2025-06-03 14:13:56 -0600

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'angularFrontend';
+export class AppComponent {
+  nombre = '';
+  metodo = 'GET'; // valor inicial
+  respuesta: any;
+
+  constructor(private http: HttpClient) {}
+
+  obtenerSaludo() {
+    if (this.metodo === 'GET') {
+      const params = new HttpParams().set('nombre', this.nombre);
+      this.http.get<any>('http://localhost:8000/api/saludos', { params })
+        .subscribe({
+          next: data => this.respuesta = data,
+          error: () => this.respuesta = { mensaje: 'Error al consultar la API' }
+        });
+    } else {
+      const body = { nombre: this.nombre };
+      this.http.post<any>('http://localhost:8000/api/saludos', body)
+        .subscribe({
+          next: data => this.respuesta = data,
+          error: () => this.respuesta = { mensaje: 'Error al consultar la API' }
+        });
+    }
+  }
 }
